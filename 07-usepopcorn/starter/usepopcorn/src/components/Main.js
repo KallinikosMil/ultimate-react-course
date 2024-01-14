b0b73a012002bea2ab7b4f8cb77a2a7f78412554
Kallinikos Mylonakis <mylonakis.kallinikosandreas@nbg.gr>
2024-01-14 14:38:10 +0200

    usePopCorn Start

---
+++ 07-usepopcorn/starter/usepopcorn/src/components/Main.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import ListBox from './ListBox';
+import WatchedBox from './WatchedBox';
+
+
+const Main = ({ tempMovieData, tempWatchedData }) => {
+  return (
+    <main className="main">
+      <ListBox tempMovieData={tempMovieData} />
+      <WatchedBox tempWatchedData={tempWatchedData} />
+    </main>
+  );
+};
+
+export default Main;
